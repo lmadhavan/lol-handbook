e5dd800b54bacd97cf393d41c436e4a52433419c
lmadhavan <lmadhavan@users.noreply.github.com>
2016-05-01 13:10:21 -0700

    Add progress indicators to pages

--- LolHandbook/Converters/VisibilityConverter.cs
+++ LolHandbook/Converters/VisibilityConverter.cs
@@ -9,18 +9,30 @@ namespace LolHandbook.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
+            bool visible = IsVisible(value);
+
+            if (parameter as string == "Invert")
+            {
+                visible = !visible;
+            }
+
+            return visible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private static bool IsVisible(object value)
+        {
             if (value == null)
             {
-                return Visibility.Collapsed;
+                return false;
             }
 
             ICollection collection = value as ICollection;
             if (collection != null && collection.Count == 0)
             {
-                return Visibility.Collapsed;
+                return false;
             }
 
-            return Visibility.Visible;
+            return true;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
