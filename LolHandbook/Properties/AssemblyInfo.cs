1c09e90557020b8bf7f85f14a1b7a3e61a044088
Madhavan <lmadhavan@users.noreply.github.com>
2021-08-15 16:16:12 -0700

    Manifest updates for v1.6

--- LolHandbook/Properties/AssemblyInfo.cs
+++ LolHandbook/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Madhavan Lakshminarayanan")]
 [assembly: AssemblyProduct("League Handbook")]
-[assembly: AssemblyCopyright("©  2019 Madhavan Lakshminarayanan")]
+[assembly: AssemblyCopyright("©  2021 Madhavan Lakshminarayanan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
 [assembly: ComVisible(false)]
